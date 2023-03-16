404544b4450621cc86eaff229b698636bd246de3
pettei47 <kitagawa.teppei.33c@gmail.com>
2023-03-17 00:03:13 +0900

    マージインサートソートを実装

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,88 +1,75 @@
 #include "PmergeMe.hpp"
 
 PmergeMe::PmergeMe(int count, char **arg) {
-  _valid = _validArgs(arg);
-  _countElem = count;
+	_valid = _validArgs(arg);
+	_countElem = count;
 }
 
 PmergeMe::~PmergeMe() {}
 
 bool  PmergeMe::_validArgs(char **arg) {
-  int i = 0;
-  while (arg[i]) {
-    int j = 0;
-    while (arg[i][j]) {
-      if (!std::isdigit(arg[i][j]))
-        return false;
-      j++;
-    }
-    long num = std::strtol(arg[i],NULL, 10);
-    if (num < 1)
-      return false;
-    if (_after.find(num) != _after.end())
-      return false;
-    _before.push_back(num);
-    _beforeList.push_back(num);
-    _after.insert(num);
-    i++;
-  }
-  return true;
+	int i = 0;
+	while (arg[i]) {
+		int j = 0;
+		while (arg[i][j]) {
+			if (!std::isdigit(arg[i][j]))
+				return false;
+			j++;
+		}
+		long num = std::strtol(arg[i],NULL, 10);
+		if (num < 1)
+			return false;
+		if (_after.find(num) != _after.end())
+			return false;
+		_before.push_back(num);
+		_beforeDeque.push_back(num);
+		_after.insert(num);
+		i++;
+	}
+	return true;
 }
 
 void  PmergeMe::_putBefore() {
-  std::cout << "Before: ";
-  for (int i = 0; i < _countElem; i++)
-    std::cout << " " << _before[i];
-  std::cout << std::endl;
+	std::cout << "Before: ";
+	for (int i = 0; i < _countElem; i++)
+		std::cout << " " << _before[i];
+	std::cout << std::endl;
 }
 
 void  PmergeMe::_putAfter() {
-  std::cout << "After:  ";
-  std::set<long>::iterator itr = _after.begin();
-  for (; itr != _after.end(); itr++)
-    std::cout << " " << *itr;
-  std::cout << std::endl;
+	std::cout << "After:  ";
+	std::set<long>::iterator itr = _after.begin();
+	for (; itr != _after.end(); itr++)
+		std::cout << " " << *itr;
+	std::cout << std::endl;
 }
 
-void  PmergeMe::_sortAsVector() {
-  timespec start;
-  timespec end;
-  ::clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-  std::sort(_before.begin(), _before.end());
-  ::clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-  double runSec = end.tv_sec - start.tv_sec;
-  double runNSec = end.tv_nsec - start.tv_nsec;
-  _vecTime = (runSec * 1000000L) + (runNSec / 1000);
-  std::cout << "Time to process a range of "
-            << _countElem
-            << " elements with std::vector : "
-            << _vecTime << " us"
-            << std::endl;
-}
-
-void  PmergeMe::_sortAsList() {
-  timespec start;
-  timespec end;
-  ::clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-  _beforeList.sort();
-  ::clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-  double runSec = end.tv_sec - start.tv_sec;
-  double runNSec = (end.tv_nsec - start.tv_nsec);
-  _listTime = (runSec * 1000000L) + (runNSec / 1000);
-  std::cout << "Time to process a range of "
-          << _countElem
-          << " elements with std::list : "
-          << _listTime << " us"
-          << std::endl;
+void	PmergeMe::_putSortTime(std::string containerName) {
+	timespec start;
+	timespec end;
+	::clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+	if (containerName == "vector")
+		_mergeInsertionSort(_before, 0, _before.size() - 1);
+	else
+		_mergeInsertionSort(_beforeDeque, 0, _before.size() - 1);
+	::clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+	double runSec = end.tv_sec - start.tv_sec;
+	double runNSec = end.tv_nsec - start.tv_nsec;
+	double sortTime = (runSec * 1000000L) + (runNSec / 1000);
+	std::cout << "Time to process a range of "
+						<< _countElem
+						<< " elements with std::" << containerName << " : "
+						<< sortTime << " us"
+						<< std::endl;
 }
 
 void  PmergeMe::exec() {
-  _putBefore();
-  _putAfter();
-  _sortAsVector();
-  _sortAsList();
+	_putBefore();
+	_putAfter();
+	_putSortTime("vector");
+	_putSortTime("deque");
 }
 
 bool  PmergeMe::get_valid() {
-  return _valid;
+	return _valid;
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -2,18 +2,18 @@
 # define PMERGEME_HPP
 # include <iostream>
 # include <vector>
-# include <list>
+# include <deque>
 # include <set>
 
 class PmergeMe
 {
 private:
-  std::vector<long> _before;
-	std::list<long>		_beforeList;
-  std::set<long> 		_after;
+	std::vector<long> _before;
+	std::deque<long>	_beforeDeque;
+	std::set<long> 		_after;
 	int								_countElem;
 	double						_vecTime;
-	double						_listTime;
+	double						_dequeTime;
 	bool							_valid;
 	PmergeMe(PmergeMe const &);
 	PmergeMe		&operator=(PmergeMe const &);
@@ -21,8 +21,66 @@ private:
 	bool	_validArgs(char **arg);
 	void	_putBefore();
 	void	_putAfter();
-	void	_sortAsVector();
-	void	_sortAsList();
+	void	_putSortTime(std::string containerName);
+
+	template<typename T>
+	void	_insertionSort(T& container, int p, int q) {
+		for (int i = p; i < q; i++) {
+			int tempVal = container[i + 1];
+			int j = i + 1;
+			while (j > p && container[j - 1] > tempVal) {
+					container[j] = container[j - 1];
+					j--;
+			}
+			container[j] = tempVal;
+		}
+	}
+
+	template<typename T>
+	T	_copyOfRange(T& container, int start, int end) {
+		T	ret;
+		for (int i = start; i < end; i++) {
+			ret.push_back(container[i]);
+		}
+		return ret;
+	}
+
+	template<typename T>
+	void _merge(T& container, int p, int q, int r) {
+		int n1 = q - p + 1;
+		int n2 = r - q;
+		T LA = _copyOfRange(container, p, q + 1);
+		T RA = _copyOfRange(container, q + 1, r + 1);
+		int RIDX = 0;
+		int LIDX = 0;
+		for (int i = p; i < r - p + 1; i++) {
+				if (RIDX == n2) {
+						container[i] = LA[LIDX];
+						LIDX++;
+				} else if (LIDX == n1) {
+						container[i] = RA[RIDX];
+						RIDX++;
+				} else if (RA[RIDX] > LA[LIDX]) {
+						container[i] = LA[LIDX];
+						LIDX++;
+				} else {
+						container[i] = RA[RIDX];
+						RIDX++;
+				}
+		}
+	}
+
+	template<typename T>
+	void	_mergeInsertionSort(T& container, int p, int r) {
+		if (r - p > 5) {
+				int q = (p + r) / 2;
+				_mergeInsertionSort(container, p, q);
+				_mergeInsertionSort(container, q + 1, r);
+				_merge(container, p, q, r);
+		} else {
+				_insertionSort(container, p, r);
+		}
+	}
 
 public:
 	PmergeMe(int count, char **arg);
